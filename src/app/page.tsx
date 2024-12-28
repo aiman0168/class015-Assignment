7f8c1c6716b6925648d0f68099c6cfb7ce9c1fc4
Umm e Aiman <160779331+aiman0168@users.noreply.github.com>
2024-12-28 10:41:22 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
-import Link from "next/link"
+import Link from "next/link";
+interface User {
+  id: number;
+  name: string;
+}
 export default async function Home() {
-const url = await fetch("https://jsonplaceholder.typicode.com/users")
-const response = await url.json()
-console.log(response)
-return (
+  const url = await fetch("https://jsonplaceholder.typicode.com/users");
+  const response: User[] = await url.json();
+  console.log(response);
+  return (
     <main>
       {
-        response.map((user: any) => (
-          <div>
+        response.map((user) => (
+          <div key={user.id}>
             <Link href={`${user.id}`}>
               <h1 className="text-4xl mb-4">
                 {user.name}
