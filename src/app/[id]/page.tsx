b5f7eb8166c82f7b581abccb55928eceb65ce962
Umm e Aiman <160779331+aiman0168@users.noreply.github.com>
2024-12-28 10:42:49 -0800

    Update page.tsx

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,14 +1,23 @@
-const DynamicUser = async (props: any) => {
-    console.log(props);
-    const url = await fetch(`https://jsonplaceholder.typicode.com/users/${props.params.id}`);
-    const response = await url.json();
-    return (
-      <div className="p-4">
-        <h1 className="text-2xl font-bold mb-2">User Details</h1>
-        <p className="text-gray-700"><strong>Name:</strong> {response.name}</p>
-        <p className="text-gray-700"><strong>Phone:</strong> {response.phone}</p>
-      </div>
-    );
+interface User {
+  id: number;
+  name: string;
+  phone: string;
+}
+interface Props {
+  params: {
+    id: string;
   };
-  
-  export default DynamicUser;  
+}
+const DynamicUser = async (props: Props) => {
+  console.log(props);
+  const url = await fetch(`https://jsonplaceholder.typicode.com/users/${props.params.id}`);
+  const response: User = await url.json();
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-2">User Details</h1>
+      <p className="text-gray-700"><strong>Name:</strong> {response.name}</p>
+      <p className="text-gray-700"><strong>Phone:</strong> {response.phone}</p>
+    </div>
+  );
+};
+export default DynamicUser;
